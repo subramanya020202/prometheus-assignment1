00b2f14393037c6891fffd41f92b58da1824bc5f
subramanya020202 <subramanya.r@sigmoidanalytics.com>
2024-12-05 14:05:15 +0530

    initialize

--- README.md
+++
@@ -1,9 +0,0 @@
-Export the metrics (like request per second, memory usage, cpu usage etc) in the existing mini project given to Interns
-
-Install Prometheus and Grafana using Docker (with docker-compose)
-
-Configure prometheus (scrape configs) such way that it can scrape the metrics from default metric path of the application job
-
-Validate the entire configuration to check if the data is coming or not in Prometheus UI
-
-Create the Dashboards in Grafana on top of the metrics exported by adding the Prometheus as a Datasource.
--- app/app.py
+++ app/app.py
@@ -1,18 +1,17 @@
 from flask import Flask, render_template, request
 from prometheus_client import start_http_server, Gauge
-import psutil  # For getting memory and CPU usage
+import psutil  
 
 app = Flask(__name__)
 
-# Define Prometheus metrics
 REQUEST_COUNT = Gauge('app_request_count', 'Total request count')
 CPU_USAGE = Gauge('app_cpu_usage', 'CPU usage in percent')
 MEMORY_USAGE = Gauge('app_memory_usage', 'Memory usage in percent')
 
-# Dummy users for login
+
 users = {
     'Sigmoid': 'sigmoid',
-    'Deepak': 'deepak'
+    'subbu': 'subbu'
 }
 
 @app.route('/')
@@ -34,15 +33,15 @@ def login():
 
 @app.route('/metrics')
 def metrics():
-    # Collect metrics for Prometheus
-    REQUEST_COUNT.inc()  # Increment request count
-    CPU_USAGE.set(psutil.cpu_percent())  # Set CPU usage
-    MEMORY_USAGE.set(psutil.virtual_memory().percent)  # Set Memory usage
+    
+    REQUEST_COUNT.inc()  
+    CPU_USAGE.set(psutil.cpu_percent())  
+    MEMORY_USAGE.set(psutil.virtual_memory().percent)  
     
     return "Metrics exposed!"
 
 if __name__ == '__main__':
-    # Start the Prometheus metrics server
-    start_http_server(8000)  # Expose metrics on port 8000
-    app.run(host='0.0.0.0', port=5000)  # Flask app runs on port 5000
+    
+    start_http_server(8000)  
+    app.run(host='0.0.0.0', port=5000)  
 
